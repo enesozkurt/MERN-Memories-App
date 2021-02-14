8780ad7b31927f62644870b991bf9422f8a5e9d3
enesokurt <enes.ozkurt4@gmail.com>
2021-02-14 15:44:30 +0300

    like post and responsive view edits

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,7 +5,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment'
 import { useDispatch } from "react-redux";
-import { deletePost } from "../../../redux/actions/posts";
+import { deletePost, likePost } from "../../../redux/actions/posts";
 
 import useStyles from './styles';
 
@@ -33,12 +33,12 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" />
-                    Like
+                    &nbsp; Like &nbsp;
                     {post.likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
+import {Grid} from "@material-ui/core";
+import React from "react";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
     appBar: {
         borderRadius: 15,
         margin: '30px 0',
@@ -15,4 +17,9 @@ export default makeStyles(() => ({
     image: {
         marginLeft: '15px',
     },
+    [theme.breakpoints.down('sm')] : {
+        mainContainer: {
+            flexDirection: "column-reverse"
+        }
+    }
 }));
